d0e72f0ebf7c3c14eaf3c725f70b1fa946af9e55
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-03-28 05:13:19 +0000

    New Change of idea -- creating a visual layout of the API

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var people;
+let request = "https://www.swapi.tech/api/people/2"
 
-fetch("https://www.swapi.tech/api/planets/1/")
-.then(res => res.json())
-.then(data => {people = data})
-.catch(err => console.error(err))
-
-console.log(people)
+fetch(request)
+  .then((response) => {
+    return response.json();
+  })
+  .then( (data) => {
+    let p = document.getElementById("text");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.result.properties.name);
+  })
