f2ec923363872173f664ea6ebf0bffb28a52bd5d
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-03-29 05:13:26 +0000

    Added links from pictures in home page and added in mock tables

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let request = "https://www.swapi.tech/api/people/2"
+let request = "https://www.swapi.tech/api/people/1"
 
 fetch(request)
   .then((response) => {
     return response.json();
   })
   .then( (data) => {
-    let p = document.getElementById("text");
+    let p = document.getElementById("Name");
     console.log(data);
     p.innerHTML = JSON.stringify(data.result.properties.name);
   })
