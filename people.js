f309bd5300515a0bff1c791182845c0b41b54b07
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-03-30 05:01:07 +0000

    It seems that the table in People.html doesn't always load properly... why?

--- people.js
+++ people.js
@@ -6,10 +6,8 @@ async function getPeople(){
   for(i=0;i<data.results.length;i++) {
     const res = await fetch(data.results[i].url);
     const person=await res.json();
-    console.log(person);
 
-    temp += "<tr>";
-    temp += "<td>" + person.result.properties.name + "</td><td>" + person.result.properties.height + "</td><td>" + person.result.properties.mass + "</td><td>" + person.result.properties.hair_color + "</td><td>" + person.result.properties.skin_color + "</td><td>" + person.result.properties.eye_color + "</td><td>" + person.result.properties.birth_year + "</td><td>" + person.result.properties.gender + "</td></tr>";
+    temp += "<tr><td>" + person.result.properties.name + "</td><td>" + person.result.properties.height + "</td><td>" + person.result.properties.mass + "</td><td>" + person.result.properties.hair_color + "</td><td>" + person.result.properties.skin_color + "</td><td>" + person.result.properties.eye_color + "</td><td>" + person.result.properties.birth_year + "</td><td>" + person.result.properties.gender + "</td></tr>";
   }
   document.getElementById("table_ppl").innerHTML = temp;
 }
